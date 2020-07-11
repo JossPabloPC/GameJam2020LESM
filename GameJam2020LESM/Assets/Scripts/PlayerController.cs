76ac75d8500c22d49da6fb39457c7f6ba55fd932
mariana-gtz <0211757@up.edu.mx>
2020-07-11 17:04:04 -0500

    Rat animation
    Animamos a la rata
    *Hay una falla en el giro a la izquierda

--- GameJam2020LESM/Assets/Scripts/PlayerController.cs
+++ GameJam2020LESM/Assets/Scripts/PlayerController.cs
@@ -1,59 +1,49 @@
-﻿using Microsoft.Win32.SafeHandles;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class RatPlayer : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rBody;
     public float speed;
     public bool isIdle;
+    private int power;
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        power = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Abajo
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            Debug.Log("Abajo");
             rBody.transform.Translate(Vector2.down * speed * Time.deltaTime, Space.World);
-            rBody.transform.eulerAngles = new Vector3(0,0,180);
+            rBody.transform.eulerAngles = new Vector3(0, 0, -90);
         }
         else if (Input.GetKey(KeyCode.UpArrow))
         {
-            Debug.Log("Arriba");
-
+            //Arriba
             rBody.transform.Translate(Vector2.up * speed * Time.deltaTime, Space.World);
-            rBody.transform.eulerAngles = new Vector3(0,0,0); 
+            rBody.transform.eulerAngles = new Vector3(0, 0, 90);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("Der");
-
+            //Derecha
             rBody.transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);
-            transform.eulerAngles = new Vector3(0, 0,-90);
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("Izq");
-
+            //Izquierda
             rBody.transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
-            rBody.transform.eulerAngles = new Vector3(0,0,90);
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Queso"))
-        {
-
+            rBody.transform.eulerAngles = new Vector3(0, 0, -180);
         }
     }
 }
