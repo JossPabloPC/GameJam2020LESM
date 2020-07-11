97422a535c9629903b3d73b7e86d066feb2ad100
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2020-07-11 12:51:52 -0500

    Comit de decoys 02

---
+++ GameJam2020LESM/Assets/Scripts/QuesoBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuesoBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public bool estaComiendose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        estaComiendose = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other != null)
+        {
+            if (other.collider.CompareTag("Player") || other.collider.CompareTag("Decoy"))
+                estaComiendose = true;
+        }
+    }
+}
