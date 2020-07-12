8a25e13e698133b700e327446cff5e8598a9202f
JossPabloPC <51423507+JossPabloPC@users.noreply.github.com>
2020-07-12 08:35:47 -0500

    Timer

---
+++ GameJam2020LESM/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float speed;
+    [HideInInspector]
+    public bool timeIsUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.localScale.x >= 0)
+            gameObject.transform.localScale -= new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+}
